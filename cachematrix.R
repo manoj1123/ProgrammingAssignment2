48f7baf31f9bc7e35c594b62f7b8409619a70c89
manoj1123 <manoj.iitbombay@gmail.com>
2014-10-23 15:16:23 +0530

    commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,45 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) 
+{
 
+  i<-NULL
+  #set the initial matrix
+  set<-function(matrix){
+    
+    m<<-matrix
+    i<<-NULL
+  }
+  #set matrix done, how to get the matrix now
+  
+  get<-function{m}
+  #now inverse the matrix
+  setInverse<-function (inverseMatrix){i<<-inverseMatrix}
+  #now get the inverse of matrix
+  getInverse<-function (){ i }
+  
+  #copy the list of methods from RD Peng program
+  list(set=set,get=get,setInverse=setInverse, getInverse=getInverse)
+  
+  
 }
 
 
 ## Write a short comment describing this function
-
+#cachesolve should retrieve the inverse of the function to avoid recalculation
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getInverse()
+  
+  if(!is.null(m)){
+    
+    message("getting cached data")
+    return(m)
+}
+
+matrixData<-x$get()
+m<-solve(data)%*% matrixData
+x$setInverse(m)
+m
 }
